a33f35f46fb97f976e2b1ca1994b1ace5a913608
Shrirang Joshi <51706802+shriI950@users.noreply.github.com>
2026-09-11 16:08:33 +0530

    Create bday_messages.py
    Created file as per the requirement CODEX challlenge.

---
+++ bday_messages.py
@@ -0,0 +1,15 @@
+# bday messages file
+
+# Write code below 💖
+
+import random
+
+bday_messages = [
+  'Hope you have a very Happy Birthday! 🎈',
+  "It's your special day – get out there and celebrate! 🎉",
+  'You were born and the world got better – everybody wins! 🥳',
+  'Have lots of fun on your special day! 🎂',
+  'Another year of you going around the sun! 🌞'
+]
+
+random_message = random.choice()
