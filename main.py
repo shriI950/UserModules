f1d2fe5c3f87769b0f66bd52c2fe73c3e622da98
Shrirang Joshi <51706802+shriI950@users.noreply.github.com>
2026-09-11 16:19:26 +0530

    Create main.py
    Creating main file for bday_messages program. Importing bday_messages module in main file.

---
+++ main.py
@@ -0,0 +1,15 @@
+# Write code below 💖
+
+import datetime, bday_messages
+
+today = datetime.date.today()
+next_birthday = datetime.date(2026, 10, 8)
+
+days_away = next_birthday - today
+
+if today == next_birthday:
+  print(bday_messages.random_message)
+else:
+  print(f"My next birthday is {days_away} days away!")
+
+# CODEX answer
